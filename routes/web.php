ec85f5a27785c44e48010bf179b8acce634037ba
ghazalia <ghaz745@gmail.com>
2021-06-22 07:30:46 +0800

    add admin auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+require 'admin.php';
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::view('/admin', 'admin.dashboard.index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
