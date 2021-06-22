d506eb26608a76a64711be001b7720fcdb1757cc
ghazalia <ghaz745@gmail.com>
2021-06-22 16:40:27 +0800

    add brandcontroller reposervice

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Contract\CategoryContract;
+use App\Repositories\CategoryRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    protected $repositories = [
+        CategoryContract::class         =>          CategoryRepository::class,
+        AttributeContract::class        =>          AttributeRepository::class,
+        BrandContract::class            =>          BrandRepository::class,
+
+    ];
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        foreach ($this->repositories as $interface => $implementation) {
+            $this->app->bind($interface, $implementation);
+        }
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\BrandController;
 
 Route::group(['prefix' => 'admin'], function () {
   Route::get('login', [LoginController::class, 'showLoginForm'])->name('admin.login');
@@ -13,4 +14,14 @@ Route::group(['middleware' => ['auth:admin']], function () {
   Route::get('/', function () {
     return view('admin.dashboard.index');
   })->name('admin.dashboard');
+});
+
+Route::group(['prefix'  =>   'brands'], function () {
+
+  Route::get('/', [BrandController::class, 'index'])->name('admin.brands.index');
+  Route::get('/create', [BrandController::class, 'create'])->name('admin.brands.create');
+  Route::post('/store', [BrandController::class, 'store'])->name('admin.brands.store');
+  Route::get('/{id}/edit', [BrandController::class, 'edit'])->name('admin.brands.edit');
+  Route::post('/update', [BrandController::class, 'update'])->name('admin.brands.update');
+  Route::get('/{id}/delete', [BrandController::class, 'delete'])->name('admin.brands.delete');
 });
